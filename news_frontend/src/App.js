ed2cd44b24a7a0da85cc4a14c48327594bba9307
makachikava <chikavamakanaka@gmail.com>
2023-03-27 12:35:07 -0700

    bookmark show completed

--- news_frontend/src/App.js
+++ news_frontend/src/App.js
@@ -1,10 +1,43 @@
+import react, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-import './App.css';
+const App = () => {
 
-function App() {
+let [bookmarks, setBookmarks] = useState([])
+
+// =========== GET BOOKMARKED ARTICLES ============== //
+
+const getBookmarks = () =>{
+  axios
+    .get('http://localhost:8000/articles')
+    .then((res)=>{
+      setBookmarks(res.data)
+      console.log(res.data)
+    })
+}
+
+useEffect(()=>{
+  getBookmarks()
+}, [])
+
   return (
     <>
     <h1>News App</h1>
+
+    <h1>Bookmarks</h1>
+    
+    {bookmarks.map((bookmark)=>{
+      return(
+        <div className='bookmark' key={bookmark.id}>
+          <img src= {bookmark.url}/>
+          <h3>{bookmark.title}</h3>
+          <h5>written by: {bookmark.author}</h5>
+          <p>{bookmark.description}</p>
+          <h5>Published at: {bookmark.publishedAt}</h5>
+        </div>
+      )
+    })}
+    
     </>
   );
 }
