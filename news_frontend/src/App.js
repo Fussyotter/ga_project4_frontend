9b87292afe41b2e11d79dc977b39124b72e6c8e6
dejtes <dejenaref@gmail.com>
2023-03-27 19:02:13 -0400

    update route and component completed

--- news_frontend/src/App.js
+++ news_frontend/src/App.js
@@ -1,65 +1,65 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import CreateBookmark from './components/CreateBookmark.jsx'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CreateBookmark from "./components/CreateBookmark.jsx";
+import EditBookmark from "./components/EditBookmark";
 
 const App = () => {
+  let [bookmarks, setBookmarks] = useState([]);
 
-let [bookmarks, setBookmarks] = useState([])
+  // =========== GET BOOKMARKED ARTICLES ============== //
 
-// =========== GET BOOKMARKED ARTICLES ============== //
+  const getBookmarks = () => {
+    axios.get("http://localhost:8000/articles").then((res) => {
+      setBookmarks(res.data);
+      console.log(res.data);
+    });
+  };
 
-const getBookmarks = () =>{
-  axios
-    .get('http://localhost:8000/articles')
-    .then((res)=>{
-      setBookmarks(res.data)
-      console.log(res.data)
-    })
-}
+  // =============== CREATE BOOKMARK ============ //
 
+  const handleCreate = (newBookmark) => {
+    axios.post("http://localhost:8000/articles", newBookmark).then((res) => {
+      console.log(res);
+      getBookmarks();
+    });
+  };
 
-// =============== CREATE BOOKMARK ============ //
+  // =============== UPDATE BOOKMARK ============ //
+  const handleUpdate = (editBookmark) => {
+    console.log(editBookmark);
+    axios
+      .put(`http://localhost:8000/articles/${editBookmark.id}`, editBookmark)
+      .then((res) => {
+        getBookmarks();
+      });
+  };
 
-const handleCreate = (newBookmark) =>{
-  axios
-    .post('http://localhost:8000/articles', newBookmark)
-    .then((res)=>{
-      console.log(res)
-      getBookmarks()
-    })
-}
-
-
-useEffect(()=>{
-  getBookmarks()
-}, [])
+  useEffect(() => {
+    getBookmarks();
+  }, []);
 
   return (
     <>
-    <h1>News App</h1>
+      <h1>News App</h1>
 
-    <h1>Bookmarks</h1>
+      <h1>Bookmarks</h1>
 
-    
-    <CreateBookmark handleCreate={handleCreate}/>
+      <CreateBookmark handleCreate={handleCreate} />
 
-    {bookmarks.map((bookmark)=>{
-      return(
-        <div className='bookmark' key={bookmark.id}>
-          <img src= {bookmark.url}/>
-          <h3>{bookmark.title}</h3>
-          <h5>written by: {bookmark.author}</h5>
-          <p>{bookmark.description}</p>
-          <h5>Published at: {bookmark.publishedAt}</h5>
-        </div>
-      )
-    })}
-    
+      {bookmarks.map((bookmark) => {
+        return (
+          <div className="bookmark" key={bookmark.id}>
+            <img src={bookmark.url} />
+            <h3>{bookmark.title}</h3>
+            <h5>written by: {bookmark.author}</h5>
+            <p>{bookmark.description}</p>
+            <h5>Published at: {bookmark.publishedAt}</h5>
+            <EditBookmark handleUpdate={handleUpdate} bookmarks={bookmarks} />
+          </div>
+        );
+      })}
     </>
   );
-}
-
-
+};
 
 export default App;
